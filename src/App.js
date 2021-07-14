599bc07ec61642577fdc6196c029478a373f3dda
mmilton <murray.l.milton@outlook.com>
2021-07-14 10:36:17 +0200

    dynamically passed and created components in between functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+function Header(props) {
+  console.log(props)
+  return (
+    <header>
+      <h1>{props.name}'s Sweet Sensations</h1>
+    </header>
+  );
+}
+function Main(props) {
+  return (
+    <section>
+      <p>We serve {props.adjective} sweet sensational tasting deserts.</p>
+    </section>
+  );
+}
+function Footer(props){
+  return (
+    <footer>
+      <p>Since {props.year}</p>
+    </footer>
+
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Horacio"/>
+      <Main adjective="Amazing!"/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
