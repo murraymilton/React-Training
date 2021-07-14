21075b8f16041a371b09edf0231d3f14db86bf21
mmilton <murray.l.milton@outlook.com>
2021-07-14 14:39:29 +0200

    created a reducer for a toggle function, that takes a current state and returns a new state.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 import "./App.css";
 
 function App() {
-  const [emotion, setEmotion] = useState("Happy Happy Joy Joy");
-  console.log(emotion)
-    return (
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  );
+
+  
+  return (
     <>
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("happy happy joy joy")}>Happy</button>
-      <button onClick={() => setEmotion("Frustrated")}>
-        Frustrated
-        </button>
-        <button onClick={() => setEmotion("enthusiastic")}>
-         Enthusiastic
-        </button>
-        <button onClick={() => setEmotion("motivated")}>
-        Motivated
-        </button>
-        <button onClick={() => setEmotion("content")}>
-          Content
-        </button>
+      <input type="checkbox"
+       value={checked}  
+      onChange={toggle}
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
-    );
-  }
+  ); 
+}
 export default App;
+
+
+// Using reducer to take a current state and return a new state.
+
+
+
+
+
+// function App() {
+//   const [emotion, setEmotion] = useState("Happy Happy Joy Joy");
+//   const [secondary, setSecondary] = useState("tired")
+
+//   useEffect(() => {
+//     console.log(`It's ${emotion} around here!`);
+//   }, [emotion]);
+
+//   useEffect(() => {
+//     console.log(`It's ${secondary} around here!`);
+//   }, [secondary]);
+//     return (
+//     <>
+//       <h1>Current emotion is {emotion} and {secondary}</h1>
+//       <button onClick={() => setEmotion("happy happy joy joy")}>
+//         Make Happy</button>
+//       <button onClick={() => setSecondary("crabby")}>
+//       Make crabby
+//       </button>
+//       <button onClick={() => setEmotion("Frustrated")}>
+//         Frustrated
+//         </button>
+//         <button onClick={() => setEmotion("enthusiastic")}>
+//          Enthusiastic
+//         </button>
+//         <button onClick={() => setEmotion("motivated")}>
+//         Motivated
+//         </button>
+//         <button onClick={() => setEmotion("content")}>
+//           Content
+//         </button>
+//     </>
+//     );
+//   }
+// export default App;
